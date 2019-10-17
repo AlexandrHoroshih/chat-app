00465bf4e565d4a38f87e9b28c7205bd3692a654
Alexander Khoroshikh <xad71rus@gmail.com>
2019-10-17 23:32:20 +0300

    refactor(app/layout): removed unused React.Fragment

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,11 @@ import './app.css';
 
 const App: React.FC = () => {
   return (
-    <>
-      <main className="layout">
-        <aside className="sidebar">Users</aside>
-        <section className="chat">message</section>
-        <section className="input">input</section>
-      </main>
-    </>
+    <main className="layout">
+      <aside className="sidebar">Users</aside>
+      <section className="chat">message</section>
+      <section className="input">input</section>
+    </main>
   );
 };
 

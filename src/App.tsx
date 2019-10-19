9c0e66bebe0ecb3e80d7b90188e37d2d544b22f6
Alexander Khoroshikh <xad71rus@gmail.com>
2019-10-19 22:42:49 +0300

    feat(styles): added normalize css

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import './app.css';
 const App: React.FC = () => {
   return (
     <main className="layout">
-      <aside className="sidebar">Users</aside>
+      <aside className="sidebar">aside</aside>
       <section className="chat">message</section>
       <section className="input">input</section>
     </main>

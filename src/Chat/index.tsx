a225557196c8364e0882b185c23956e23c0155b5
Alexander Khoroshikh <xad71rus@gmail.com>
2019-10-27 22:44:14 +0300

    fix(chat): messages renders in right order

--- src/Chat/index.tsx
+++ src/Chat/index.tsx
@@ -10,13 +10,16 @@ const Chat: React.FC = observer(() => {
   return (
     <div className={classes.chatWrapper}>
       {store.messages.map((message: IMessage, i: number) => {
+        const { id, username, text, datetime } = store.messages[
+          store.messages.length - 1 - i
+        ];
         return (
           <Message
             key={i}
-            isMyMessage={store.myUser.id === message.id}
-            userName={message.username}
-            message={message.text}
-            datetime={message.datetime}
+            isMyMessage={store.myUser.id === id}
+            userName={username}
+            message={text}
+            datetime={datetime}
           />
         );
       })}

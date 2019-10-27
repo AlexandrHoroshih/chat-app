a3d13c46507dc402852bec4569f336bd5c105fc5
Alexander Khoroshikh <xad71rus@gmail.com>
2019-10-27 23:37:52 +0300

    feat(back): basic websockets backend setup

--- back/src/index.ts
+++ back/src/index.ts
@@ -1,3 +1,41 @@
-const testFunc = (): void => console.log('hi');
+const ws = require('ws');
 
-testFunc();
+const wss = new ws().Server({ port: 8989 });
+
+enum WsEvents {
+  CONNECTION = 'connection',
+  MESSAGE = 'message',
+  CLOSE = 'close',
+}
+
+interface User {
+  id: number;
+  username: string;
+}
+
+const users: User[] = [];
+
+const broadcast = (data, myWs) => {
+  wss.clients.forEach(client => {
+    if (client.readyState === ws.OPEN && client !== myWs) {
+      client.send(JSON.stringify(data));
+    }
+  });
+};
+
+wss.on(WsEvents.CONNECTION, ws => {
+  console.log('connected', ws);
+  ws.on(WsEvents.MESSAGE, message => {
+    console.log('message', message);
+    broadcast(
+      {
+        message: 'hi',
+      },
+      ws,
+    );
+  });
+
+  ws.on(WsEvents.CLOSE, () => {
+    console.log('closed');
+  });
+});

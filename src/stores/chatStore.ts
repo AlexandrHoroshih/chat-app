66d9231b57fda0b7388760ea8083004b123543a4
Alexander Khoroshikh <xad71rus@gmail.com>
2019-10-27 17:32:14 +0300

    feat(store): added basic actions and computed values
    added: addUser, addMessage, removeUser actions, userList computed value

--- src/stores/chatStore.ts
+++ src/stores/chatStore.ts
@@ -1,6 +1,7 @@
 import { observable, action, computed } from 'mobx';
 
 type User = {
+  id: number;
   name: string;
 };
 
@@ -15,4 +16,20 @@ export class chatStore {
   @observable isLoading: boolean = false;
   @observable users: User[] = [];
   @observable messages: Message[] = [];
+
+  @computed get userList(): string[] {
+    return this.users.map(user => user.name);
+  }
+
+  @action addUser = (user: User): void => {
+    this.users.push(user);
+  };
+
+  @action addMessage = (message: Message): void => {
+    this.messages.push(message);
+  };
+
+  @action removeUser = (user: User): void => {
+    this.users = this.users.filter(item => item.id !== user.id);
+  };
 }
